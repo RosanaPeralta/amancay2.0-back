26b201ad344ab58bb9f6408d9efad034934b3b73
Ro <rosanap0604@gmail.com>
2026-09-08 08:57:44 -0300

    refactor: remove unused exception and database migration files for products and categories

--- src/main/java/com/amancay/exceptions/DuplicateSlugException.java
+++
@@ -1,7 +0,0 @@
-package com.amancay.exceptions;
-
-public class DuplicateSlugException extends RuntimeException {
-    public DuplicateSlugException(String slug) {
-        super("A product with slug '" + slug + "' already exists");
-    }
-}
--- src/main/resources/db/migration/V3__add_price_and_stock_to_products.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE products
-    ADD COLUMN price DECIMAL(12, 2) NOT NULL DEFAULT 0,
-    ADD COLUMN stock_quantity INT NOT NULL DEFAULT 0;
